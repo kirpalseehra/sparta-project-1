ca8662f4e3b21d59d841af6798d1f0b066c8c0b8
Tech-A45 <tech-a45@Tech-A45xs-MacBook-Air.local>
2018-12-05 11:42:57 +0000

    Implemented the changing of turns so two different players can play

--- connectFour.js
+++ connectFour.js
@@ -3,12 +3,14 @@ class ConnectFour {
     this.ROWS = 6;
     this.COLS = 7;
     this.selector = selector;
+    this.player = 'redCounter';
     this.createGrid();
     this.gridEventListeners();
   }
   //creating the rows anc columns for the grid using javascript and jquery
   createGrid() {
     const board = $(this.selector);
+
     console.log(board);
     for (let x = 0; x < this.ROWS; x++) {
       const row = $('<div>').addClass('row');
@@ -23,6 +25,7 @@ class ConnectFour {
 
   gridEventListeners() {
     const board = $(this.selector);
+    const that = this;
     function findLastCell(col) {
       //get all the columns that have the same attribute data-col and make it equal to the index column col
       const cells = $(`.col[data-col='${col}']`);
@@ -40,12 +43,12 @@ class ConnectFour {
     board.on('mouseenter', '.col.empty', function () {
       const col = $(this).data('col');
       const lastEmptyCell = findLastCell(col);
-      lastEmptyCell.addClass(`nextRedHover`)
+      lastEmptyCell.addClass('next-${that.player}')
     });
 
     // when you hover elsewhere away from the cell it removes the colour from that cell
     board.on('mouseleave', '.col', function () {
-      $('.col').removeClass(`nextRedHover`);
+      $('.col').removeClass('next-${that.player}');
     });
 
     board.on('click', '.col', function () {
@@ -53,7 +56,9 @@ class ConnectFour {
       const col = $(this).data('col');
       const lastEmptyCell = findLastCell(col);
       lastEmptyCell.removeClass('empty');
-      lastEmptyCell.addClass('redCounter');
+      lastEmptyCell.addClass(that.player);
+      that.player = (that.player === 'redCounter') ? 'blackCounter' : 'redCounter';
+
     });
   }
 
