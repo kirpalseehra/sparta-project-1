eb8b6d4c1bf36880023477d9101839e173886896
Tech-A45 <tech-a45@Tech-A45xs-MacBook-Air.local>
2018-12-07 10:54:21 +0000

    created modal with instructions

--- connectFour.js
+++ connectFour.js
@@ -31,6 +31,31 @@ class ConnectFour {
 
   //all the event listeners when a user clicks or hovers over each index in the grid
   gridEventListeners() {
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function () {
+      modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
     const board = $(this.selector);
     const that = this;
     function findLastCell(col) {
@@ -77,7 +102,6 @@ class ConnectFour {
         // added applause when someone wins
         document.getElementById('applause').play();
         alert('Congratulations! You are the winner!');
-
         that.endGame = true;
       }
 
@@ -135,5 +159,7 @@ class ConnectFour {
     return GameWinVH() || GameWinDiags();
   }
 }
+
+
 // change the text on player's turn
 // styling
